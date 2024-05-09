d72368eb9fdf4bf5508439fb078238f924697540
Obada Yasser <obadayasser40@gmail.com>
2024-05-10 02:44:33 +0300

    Nour

---
+++ js/main.js
@@ -0,0 +1,73 @@
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+    /*  */
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== '') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+/*  */
+$( function() {
+    $( "#inputCheckIn" ).datepicker();
+    $( "#inputCheckOut" ).datepicker();
+    $('.level-slider').slick({
+      dots: true,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      arrows: false,
+    });
+  
+  
+    } );
+/*  */
+const navLinks = document.querySelectorAll(".nav-item").forEach((navItem) => {
+    navItem.addEventListener("click", () => {
+      document.querySelector(".nav-items").classList.toggle("active");
+    });
+  });
+
+
+
+
+
+
+
+
